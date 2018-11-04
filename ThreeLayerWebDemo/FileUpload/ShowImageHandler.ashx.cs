fe3842dbe3eb8eede6ebf4a07183dae944f4fa1a
threradPool321 <972029776@qq.com>
2018-11-04 21:42:19 +0800

    图片防盗链

--- ThreeLayerWebDemo/FileUpload/ShowImageHandler.ashx.cs
+++ ThreeLayerWebDemo/FileUpload/ShowImageHandler.ashx.cs
@@ -10,11 +10,27 @@ namespace ThreeLayerWebDemo.FileUpload
     /// </summary>
     public class ShowImageHandler : IHttpHandler
     {
+        //在返回图片的时候进行判断是否是同一个本站点过来的请求
 
         public void ProcessRequest(HttpContext context)
         {
             context.Response.ContentType = "image/jpeg";
-            context.Response.WriteFile("a.jpg");
+            //判断是否是本网站的请求，如果是进行放行，如果不是进行拒绝请求
+            
+            Uri referrerUrl = context.Request.UrlReferrer;   //客户端的url
+
+            //判断：UrlReferrer的域名和端口是否和自己端口一样
+            Uri requestUrl = context.Request.Url;
+
+            if(Uri.Compare(referrerUrl,requestUrl,UriComponents.HostAndPort,UriFormat.SafeUnescaped,StringComparison.CurrentCulture)==0)
+            {
+                context.Response.WriteFile("a.jpg");
+
+            }
+            else
+            {
+                context.Response.WriteFile("b.jpg");  //这是一个错误的图片
+            }
         }
 
         public bool IsReusable
